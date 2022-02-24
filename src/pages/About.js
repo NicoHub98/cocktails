967c21075cb45ce492827a1536d06c86e4ee0cb2
unknown <nicolud98@gmail.com>
2022-02-24 17:33:54 -0300

    ic

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis cum beatae
+        sed quaerat delectus, praesentium aliquid suscipit dolor ullam at earum
+        accusamus? Voluptas in animi eius eligendi sunt recusandae cum!
+      </p>
+    </section>
+  );
+};
+
+export default About;
